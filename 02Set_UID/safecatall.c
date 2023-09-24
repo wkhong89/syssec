d67e2fa3b52bc994a576b5c052bfa3ea7b3266e9
Won-Kee Hong <wkhong89@gmail.com>
2023-09-24 08:11:55 -0400

    ch02

---
+++ 02Set_UID/safecatall.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	char *v[3];
+
+	if (argc < 2) {
+		printf("Please type a file name.\n");
+		return 1;
+	}
+
+	v[0] = "/bin/cat";
+	v[1] = argv[1];
+	v[2] = 0;
+
+	execve(v[0], v, 0);
+
+	return 0;
+}
